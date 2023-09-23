bb8614cc0c7f242ed201b9e7b4fdbca4da3a4192
Snaehath <2020ad0556@svce.ac.in>
2023-09-23 20:49:55 +0530

    changed Quotes

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const container = document.querySelector('div');
-const trig = document.querySelector('button');
-const imageL = document.querySelector('.left');
-const imageR = document.querySelector('.right');
+const trigger = document.querySelector('button');
 
-const h1 = document.querySelector('h1');
-var quotes = ["House blowing up builds character.","All the dinosaurs feared the T-Rex.","Ever see 127 Hours?","Captain Deadpool! No, just Deadpool.","Fourth wall break inside of a fourth wall break? That's like... 16 walls!","Superhero landing! She's going to do a superhero landing!"]
+var quotes = ["Don't watch the clock, Do what it does keep going",
+                "All the dinosaurs feared the T-Rex because it worked to become top of the food chain",
+                "Everything is hard, Before it is easy",
+                "Only way is to finish is to start",
+                "Being good is not enough, You have to be best",
+                "You have two choices make excuses or make progress",
+                "Remember there is no finishing Line"]
 
-imageL.src = 'Deadpool.jfif';
-imageR.src = 'Deadpool.jfif';
 function newQuote(){
     var randomNum = Math.floor(Math.random() * (quotes.length));
     container.innerText = quotes[randomNum]; 
 }
-trig.addEventListener('click',newQuote);
+
+trigger.addEventListener('click',newQuote);
 
 
